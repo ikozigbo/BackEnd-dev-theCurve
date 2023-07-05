0ff05f49e330b5b0abbe23d3ba3195a1d7fdf6ee
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-07-05 16:13:13 +0100

    05/07

---
+++ week10/auth/server.js
@@ -0,0 +1,16 @@
+require("./config/dbConfig");
+const express = require("express");
+const router = require("./routes");
+//require("dotenv").config;
+const PORT = process.env.PORT || 5050;
+
+const app = express();
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+//app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
+});
