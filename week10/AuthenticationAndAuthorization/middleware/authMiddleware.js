b9aefb6f92faacf8e982b08c8bbb44e2733e1eac
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-07-21 19:13:36 +0100

    21/07

--- week10/AuthenticationAndAuthorization/middleware/authMiddleware.js
+++ week10/AuthenticationAndAuthorization/middleware/authMiddleware.js
@@ -1,5 +1,20 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
+// // // auth middleware for the application
+// // const userAuth = async (req, res, next)=>{
+// //     const hasAuthorization = req.headers.authorization;
+// //     if(!hasAuthorization) {
+// //         res.status(404).json({
+// //             message: 'No authorization found'
+// //         })
+// //     } else {
+// //         console.log(hasAuthorization)
+// //         const token = hasAuthorization.split(' ')[1];
+// //         const decodedToken = await jwt.verify(token, "mySecret")
+// //         req.userId = decodedToken.userId
+// //     }
+// //     next();
+// // }
 
 // // auth middleware for the application
 // const userAuth = async (req, res, next)=>{
@@ -8,38 +23,78 @@ const jwt = require('jsonwebtoken');
 //         res.status(404).json({
 //             message: 'No authorization found'
 //         })
-//     } else {
-//         console.log(hasAuthorization)
-//         const token = hasAuthorization.split(' ')[1];
-//         const decodedToken = await jwt.verify(token, "mySecret")
-//         req.userId = decodedToken.userId
 //     }
-//     next();
+//     const token = hasAuthorization.split(' ')[1];
+//     try {
+//         console.log(hasAuthorization)
+//         const decodedToken = await jwt.verify(token, "mySecret")
+//         req.user = JSON.stringify(decodedToken)
+//         req.userId = decodedToken.userId
+//         req.userEmail = decodedToken.email
+//         req.username = decodedToken.username
+//         next()
+//     } catch (error) {
+//         res.status(500).json({
+//             message: error.message
+//         })
+//     }
 // }
 
+// module.exports = {userAuth}
 
-// auth middleware for the application
-const userAuth = async (req, res, next)=>{
-    const hasAuthorization = req.headers.authorization;
-    if(!hasAuthorization) {
-        res.status(404).json({
-            message: 'No authorization found'
-        })
-    } 
-    const token = hasAuthorization.split(' ')[1];
-    try {
-        console.log(hasAuthorization)
-        const decodedToken = await jwt.verify(token, "mySecret")
-        req.user = JSON.stringify(decodedToken)
-        req.userId = decodedToken.userId
-        req.userEmail = decodedToken.email
-        req.username = decodedToken.username
-        next()
-    } catch (error) {
-        res.status(500).json({
-            message: error.message
-        })
+const jwt = require("jsonwebtoken");
+const User = require("../models/model");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+// auth middleware
+const userAuth = async (req, res, next) => {
+  try {
+    if (req.headers.authorization) {
+      const hasAuthorization = req.headers.authorization;
+      const token = hasAuthorization.split(" ")[1];
+
+      const decodedToken = await jwt.verify(token, process.env.JWT_SECRETE);
+      req.user = JSON.stringify(decodedToken);
+      req.userId = decodedToken.userId;
+      req.userEmail = decodedToken.email;
+      req.username = decodedToken.username;
+      next();
+    } else {
+      res.status(404).json({
+        message: "No authorization found",
+      });
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-module.exports = {userAuth}
+const isAdminAuthorized = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (user.isAdmin) {
+      next();
+    } else {
+      res.status(401).json({ message: "not an admin" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const isSuperAdminAuthorized = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (user.isSuperAdmin) {
+      next();
+    } else {
+      res.status(401).json({ message: "not a super admin" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { userAuth, isAdminAuthorized, isSuperAdminAuthorized };
--- week11/class-project/controller/userController.js
+++ week11/class-project/controller/userController.js
@@ -177,6 +177,7 @@ const updateUser = async (req, res) => {
         { username, email },
         { new: true }
       );
+
       res.status(200).json({ message: "user updated", updatedUser });
     } else {
       res
