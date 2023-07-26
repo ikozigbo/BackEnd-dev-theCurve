968d3e01444cbd85356024398f71648184e961b1
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-07-26 22:32:08 +0100

    26/07.

--- trippy-app/controller/userController.js
+++ trippy-app/controller/userController.js
@@ -6,36 +6,42 @@ const { genToken, decodeToken } = require("../utilities/jwt");
 
 const newUser = async (req, res) => {
   try {
-    const { firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
     const isEmail = await User.findOne({ email });
-    if (isEmail) {
-      res.status(400).json({
-        message: "email already registerd",
-      });
+    if (password === confirmPassword) {
+      if (isEmail) {
+        res.status(400).json({
+          message: "email already registerd",
+        });
+      } else {
+        const salt = bcryptjs.genSaltSync(10);
+        const hash = bcryptjs.hashSync(password, salt);
+        const user = await User.create({
+          firstName,
+          lastName,
+          email: email.toLowerCase(),
+          password: hash,
+        });
+        const token = await genToken(user._id, "30m");
+        const subject = "New User";
+        const link = `${req.protocol}://${req.get(
+          "host"
+        )}/trippy/verify/${token}`;
+        const message = `welcome onboard kindly use this ${link} to verify your account`;
+        const data = {
+          email: email,
+          subject,
+          message,
+        };
+        sendEmail(data);
+        res.status(200).json({
+          success: true,
+          user,
+        });
+      }
     } else {
-      const salt = bcryptjs.genSaltSync(10);
-      const hash = bcryptjs.hashSync(password, salt);
-      const user = await User.create({
-        firstName,
-        lastName,
-        email: email.toLowerCase(),
-        password: hash,
-      });
-      const token = await genToken(user._id, "30m");
-      const subject = "New User";
-      const link = `${req.protocol}://${req.get(
-        "host"
-      )}/trippy/verify/${token}`;
-      const message = `welcome onboard kindly use this ${link} to verify your account`;
-      const data = {
-        email: email,
-        subject,
-        message,
-      };
-      sendEmail(data);
-      res.status(200).json({
-        success: true,
-        user,
+      res.status(400).json({
+        message: "Your password and Confirm password must match",
       });
     }
   } catch (error) {
